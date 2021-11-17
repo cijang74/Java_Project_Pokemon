eba02c8939c7417ab5366f97304c563f64d494b6
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-11-17 22:07:59 +0900

    필요없는 주석 삭제

--- src/Main_Loop.java
+++ src/Main_Loop.java
@@ -7,8 +7,6 @@ public class Main_Loop
         JPanelChange win = new JPanelChange();
         win.setTitle("Pokemon");
         win.setStart_panel(new Start_Panel(win));
-//        win.setIntro_before_panel(new Intro_Before_Panel(win));
-//        win.setIntro_choose_panel(new Intro_Choose_Panel(win));
 
         // 처음 시작 패널은 start_Panel
         win.add(win.getStart_panel());
